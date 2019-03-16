3d93680447a0c7f041f91fb17c6c897e39b106fe
malyninja38 <malyninja27@gmail.com>
2019-03-16 02:24:18 +0100

    Wyświetlacz - sprawdzenie poprawności

---
+++ Test345678/Debug/Src/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/stm32f4xx_hal_msp.c \
+../Src/stm32f4xx_it.c \
+../Src/system_stm32f4xx.c \
+../Src/tm_stm32f4_lis302dl_lis3dsh.c 
+
+OBJS += \
+./Src/main.o \
+./Src/stm32f4xx_hal_msp.o \
+./Src/stm32f4xx_it.o \
+./Src/system_stm32f4xx.o \
+./Src/tm_stm32f4_lis302dl_lis3dsh.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/stm32f4xx_hal_msp.d \
+./Src/stm32f4xx_it.d \
+./Src/system_stm32f4xx.d \
+./Src/tm_stm32f4_lis302dl_lis3dsh.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/PTM/Test345678/Test345678/Inc" -I"D:/PTM/Test345678/Test345678/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/PTM/Test345678/Test345678/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/PTM/Test345678/Test345678/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/PTM/Test345678/Test345678/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
